48afd6aaabd6e7512b9849f9287b7772377da5cb
sirsean <sirsean@gmail.com>
2022-06-27 05:51:27 -0500

    new version

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import sortBy from 'sort-by';
 import './App.css';
 
 const API_URL = 'https://liquidlands-api.sirsean.workers.dev';
-const APP_VERSION = '10175a1a1';
+const APP_VERSION = '10176a1a1';
 
 const slice = createSlice({
     name: 'land-hunter',
